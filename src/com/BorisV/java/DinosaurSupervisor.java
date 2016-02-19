990b02827f4f8c81ed89ce26255bd0e4662829a6
Boris <bcarl@gmail.com>
2016-02-18 18:11:20 -0600

    Started Dinosaur program Added a welcome message

--- src/com/BorisV/java/DinosaurSupervisor.java
+++ src/com/BorisV/java/DinosaurSupervisor.java
@@ -3,7 +3,8 @@ package com.BorisV.java;
 public class DinosaurSupervisor {
 
     public static void main(String[] args) {
-	Dinosaur t_rex = new Dinosaur();
+        System.out.println("Dinousaur program");
+        Dinosaur t_rex = new Dinosaur();
         Dinosaur velociraptor = new Dinosaur();
         t_rex.roar();
         velociraptor.roar();
